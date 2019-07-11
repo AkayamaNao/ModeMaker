e0cc835ce558116869876ab2e84b8b8d646d67d3
AkayamaNao <43431233+AkayamaNao@users.noreply.github.com>
2019-07-11 20:04:52 +0900

    edit app.py

--- app.py
+++ app.py
@@ -38,3 +38,6 @@ app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 
 app.register_blueprint(view, url_prefix='/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=settings.DEBUG)
