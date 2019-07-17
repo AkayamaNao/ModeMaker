3bed1bdb0b7b69c027302f27e489fe430e3b50f7
AkayamaNao <43431233+AkayamaNao@users.noreply.github.com>
2019-07-17 14:52:08 +0900

    test ver.2

--- test_app.py
+++ test_app.py
@@ -1,20 +1,22 @@
 import os
-import tempfile
 import pytest
 import app
 
 @pytest.fixture
 def client():
-    db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
     app.app.config['TESTING'] = True
     client = app.app.test_client()
-    with app.app.app_context():
-        app.init_db()
     yield client
-    os.close(db_fd)
-    os.unlink(app.app.config['DATABASE'])
 
-def test_empty_db(client):
-    """Start with a blank database."""
-    rv = client.get('/')
-    assert b'No entries here so far' in rv.data
+def test_messages(client):
+    img_list=os.listdir('images')
+    img=img_list[0]
+
+    rv = client.post('/', data=dict(
+        img_file=img,
+        x=0,
+        y=0,
+        color='rgba(0,0,0)',
+        k=5
+    ), follow_redirects=True)
+    assert b'No entries here so far' not in rv.data
